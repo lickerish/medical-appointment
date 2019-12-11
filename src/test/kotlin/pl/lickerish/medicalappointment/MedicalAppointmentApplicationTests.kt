b87daa6580935f3d6df83beeb480846250a52286
lickerish <kacper.lakomski@gmail.com>
2019-12-11 23:01:23 +0100

    Initial Commit

---
+++ src/test/kotlin/pl/lickerish/medicalappointment/MedicalAppointmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.lickerish.medicalappointment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedicalAppointmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
