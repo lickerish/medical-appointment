5cd1ad4c3493fc1428413c2c86fa2d70181dfcff
lickerish <kacper.lakomski@gmail.com>
2019-12-12 18:46:18 +0100

    Update models with necessary annotations

--- src/main/kotlin/pl/lickerish/medicalappointment/models/Doctor.kt
+++ src/main/kotlin/pl/lickerish/medicalappointment/models/Doctor.kt
@@ -1,9 +1,16 @@
 package pl.lickerish.medicalappointment.models
 
-import javax.persistence.Id
+import javax.persistence.*
 
+@Entity
+@Table(name = "doctor")
 data class Doctor(@Id
+                  @GeneratedValue(strategy = GenerationType.IDENTITY)
+                  @Column(name = "id", updatable = false,nullable = false)
                   val id: Long,
                   val firstName: String,
                   val lastName: String,
-                  val specialization: String)
+                  val specialization: String,
+                  @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL] )
+                  @JoinColumn(name = "DOCTOR_ID")
+                  var appointments: Set<Appointment>? = null)
--- src/main/kotlin/pl/lickerish/medicalappointment/models/Patient.kt
+++ src/main/kotlin/pl/lickerish/medicalappointment/models/Patient.kt
@@ -1,9 +1,20 @@
 package pl.lickerish.medicalappointment.models
 
-import javax.persistence.Id
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import javax.persistence.*
 
+@Entity
+@Table(name = "patient")
 data class Patient(@Id
-                   val id: Long,
-                   var firstName: String,
-                   var lastName: String,
-                   var address: String)
+                   @GeneratedValue(strategy = GenerationType.IDENTITY)
+                   @Column(name = "id", updatable = false, nullable = false)
+                   val id: Long?,
+                   val firstName: String,
+                   val lastName: String,
+                   val address: String,
+                   @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL] )
+                   @JoinColumn(name = "PATIENT_ID")
+                   var appointments: Set<Appointment>? = null) {
+
+
+}
