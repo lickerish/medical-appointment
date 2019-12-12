51c149b0f2b8f5c78416c0e7208991e9c56b74be
lickerish <kacper.lakomski@gmail.com>
2019-12-12 18:47:36 +0100

    Change the way appointment is created

--- src/main/kotlin/pl/lickerish/medicalappointment/services/AppointmentService.kt
+++ src/main/kotlin/pl/lickerish/medicalappointment/services/AppointmentService.kt
@@ -1,13 +1,20 @@
 package pl.lickerish.medicalappointment.services
 
 import org.springframework.stereotype.Service
+import pl.lickerish.medicalappointment.dto.AppointmentDto
 import pl.lickerish.medicalappointment.exceptions.AppointmentNotFoundException
 import pl.lickerish.medicalappointment.models.Appointment
+import pl.lickerish.medicalappointment.models.Doctor
+import pl.lickerish.medicalappointment.models.Patient
 import pl.lickerish.medicalappointment.repositories.AppointmentRepository
+import pl.lickerish.medicalappointment.repositories.DoctorRepository
+import pl.lickerish.medicalappointment.repositories.PatientRepository
 import java.util.*
 
 @Service
-class AppointmentService(val appointmentRepository: AppointmentRepository) {
+class AppointmentService(val appointmentRepository: AppointmentRepository,
+                         val doctorRepository: DoctorRepository,
+                         val patientRepository: PatientRepository) {
     fun findAll(): List<Appointment> = this.appointmentRepository.findAll()
 
     fun findById(id: Long): Optional<Appointment> = this.appointmentRepository.findById(id)
@@ -16,7 +23,20 @@ class AppointmentService(val appointmentRepository: AppointmentRepository) {
         return appointmentRepository.findAppointmentsByPatientId(id)
     }
 
-    fun create(appointment: Appointment): Appointment = this.appointmentRepository.save(appointment)
+    fun create(appointmentDto: AppointmentDto): Appointment {
+        var doctor: Optional<Doctor> = doctorRepository.findById(appointmentDto.doctorId)
+        var patient: Optional<Patient> = patientRepository.findById(appointmentDto.patientId)
+        val appointment = Appointment(null, appointmentDto.date, appointmentDto.time, appointmentDto.place, doctor.get(), patient.get())
+        val set: Set<Appointment> = setOf(appointment)
+        doctor.get().appointments = set
+        patient.get().appointments = set
+        this.appointmentRepository.save(appointment)
+
+        this.doctorRepository.save(doctor.get())
+        this.patientRepository.save(patient.get())
+        return appointment
+
+    }
 
     fun updateAppointmentTime(appointment: Appointment, id: Long): Appointment {
         return this.appointmentRepository.findById(id).map { existingAppointment ->
