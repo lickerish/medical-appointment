c839a73bd91f6ff4618cae0e93e298e160002348
lickerish <kacper.lakomski@gmail.com>
2019-12-29 19:54:33 +0100

    Validate size of postal code

--- src/main/kotlin/pl/lickerish/medicalappointment/models/Address.kt
+++ src/main/kotlin/pl/lickerish/medicalappointment/models/Address.kt
@@ -2,6 +2,7 @@ package pl.lickerish.medicalappointment.models
 
 import javax.persistence.*
 import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 @Entity
 @Table(name = "address")
@@ -15,4 +16,5 @@ data class Address(
         val city: String,
         val country: String,
         @Pattern(regexp = "\\d{2}(-\\d{3})?", message = "Invalid zip code! Format: **-***")
+        @Size(min = 6, max = 6, message = "Postal code should be 6 character long")
         val postalCode: String)
